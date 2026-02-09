d1b6dfef6601dc013136fde4a718a4fed93f1633
senuri <senuri@gmail.com>
2026-02-09 23:29:06 +0530

    Add sign in and sign up js file

---
+++ script.js
@@ -0,0 +1,11 @@
+// Show Signup Form
+function showSignup() {
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("signup-box").style.display = "block";
+}
+
+// Show Login Form
+function showLogin() {
+    document.getElementById("signup-box").style.display = "none";
+    document.getElementById("login-box").style.display = "block";
+}
