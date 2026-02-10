cdefc4d3cbb8ae5e3e45ca45f231cefe6dc811ff
senuri <senuri@gmail.com>
2026-02-10 20:26:27 +0530

    Add user html,css,js

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function showLogin() {
     document.getElementById("signup-box").style.display = "none";
     document.getElementById("login-box").style.display = "block";
 }
+
+
+// user 
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+
+function toggleRelief(button) {
+    button.classList.toggle('active'); 
+}
+
