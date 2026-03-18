2a2aaf88cd0f1ef133b09a380ff6f3ef7fa52549
senuri <senuri@gmail.com>
2026-03-18 16:58:27 +0530

    Updated main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function showSignup() {
+
+    // Hide the login form
+    document.getElementById("login-box").style.display = "none";
+
+    // Show the signup form
+    document.getElementById("signup-box").style.display = "block";
+}
+
+function showLogin() {
+
+    // Hide the signup form
+    document.getElementById("signup-box").style.display = "none";
+
+    // Show the login form
+    document.getElementById("login-box").style.display = "block";
+}
+
+
+
+
